7c9f8454fc9430ee806fc69d69c200dcead3ab7c
Martino Facchin <m.facchin@arduino.cc>
2017-11-09 10:04:28 +0100

    passthrough - 2.2 mA

--- Projects/Multi/Applications/LoRa/AT_Slave/src/main.c
+++ Projects/Multi/Applications/LoRa/AT_Slave/src/main.c
@@ -170,6 +170,71 @@ void HW_Main_Init()
   HW_GpioInit();
 }
 
+static bool goDumb() {
+  // Setup GPIOPB12 as input
+  GPIO_InitTypeDef initStruct={0};
+  initStruct.Mode = GPIO_MODE_INPUT;
+  initStruct.Pull = GPIO_PULLUP;
+  initStruct.Speed = GPIO_SPEED_HIGH;
+
+  HW_GPIO_Init( GPIOB, GPIO_PIN_12, &initStruct );
+
+  return HW_GPIO_Read(GPIOB, GPIO_PIN_12) == 0;
+}
+
+void setupPassthrough() {
+	// input pins
+	GPIO_InitTypeDef initStruct={0};
+	initStruct.Mode = GPIO_MODE_INPUT;
+	initStruct.Pull = GPIO_NOPULL;
+	initStruct.Speed = GPIO_SPEED_HIGH;
+
+	HW_GPIO_Init( GPIOB, GPIO_PIN_13, &initStruct );
+	HW_GPIO_Init( GPIOA, GPIO_PIN_3, &initStruct );
+	HW_GPIO_Init( GPIOA, GPIO_PIN_6, &initStruct );
+	HW_GPIO_Init( GPIOB, GPIO_PIN_4, &initStruct );
+
+	// output pins
+	initStruct.Mode = GPIO_MODE_OUTPUT_PP;
+	initStruct.Pull = GPIO_NOPULL;
+
+	HW_GPIO_Init( GPIOB, GPIO_PIN_3, &initStruct );
+	HW_GPIO_Init( GPIOA, GPIO_PIN_7, &initStruct );
+	HW_GPIO_Init( GPIOA, GPIO_PIN_2, &initStruct );
+#ifdef USE_DIO0_IRQ
+	HW_GPIO_Init( GPIOB, GPIO_PIN_12, &initStruct );
+#endif
+	HW_GPIO_Init( GPIOB, GPIO_PIN_15, &initStruct );
+
+	// reset and SS pins
+	SX1276Reset();
+	HW_GPIO_Init( RADIO_NSS_PORT, RADIO_NSS_PIN, &initStruct );
+#ifdef USE_DIO0_IRQ
+	HW_GPIO_Write( RADIO_NSS_PORT, RADIO_NSS_PIN, 0 );
+#endif
+}
+
+static inline void runPassthrough() {
+	// PB13 -> PB3
+	// PA3 -> PA7
+	// PA6 -> PA2
+	// PB4 -> PB12
+
+#define PORTA_OUT_MASK ((1 << 2) | (1 << 7) | (1 << 15))
+
+	uint32_t InPortA = READ_REG(GPIOA->IDR);
+	uint32_t InPortB = READ_REG(GPIOB->IDR);
+
+	uint32_t OutPortB = (((InPortB & GPIO_PIN_13) >> 10));
+	uint32_t OutPortA = ((InPortA & GPIO_PIN_3) << 4) | ((InPortA & GPIO_PIN_6) >> 4) | ((InPortB & GPIO_PIN_12) << 3);
+
+	WRITE_REG(GPIOA->BSRR, ((OutPortA & PORTA_OUT_MASK) | ((~OutPortA & PORTA_OUT_MASK) << 16)));
+	WRITE_REG(GPIOB->BSRR, ((OutPortB & (1 << 3)) | (((~OutPortB & (1 << 3)) << 16))));
+	return;
+}
+
+
+
 LoRaMacRegion_t globalRegion = LORAMAC_REGION_EU868;
 
 int main(void)
@@ -180,6 +245,13 @@ int main(void)
   /* Configure the system clock*/
   SystemClock_Config();
 
+  /* Read SS input (GPIOPB12); if low, enter dumb mode */
+  if (goDumb()) {
+    setupPassthrough();
+	  while (1) {
+        runPassthrough();
+	}
+  }
   /* Configure the hardware*/
   HW_Init();
 
